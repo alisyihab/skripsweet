3c742fd714517e252d9e9bb14fe824cadc9c4545
alisyihab <ali.syihab@gmail.com>
2020-04-03 06:21:56 +0700

    templating adminLTE dan auth

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 2.4.0
+            </div>
+            <strong>Copyright &copy; {{ date('Y') }} .</strong> buiild with
+            <i class="fa fa-heart"></i>
+            <a href="https://instagram.com/alibelucky">
+                alibelucky
+            </a>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {}
+</script>

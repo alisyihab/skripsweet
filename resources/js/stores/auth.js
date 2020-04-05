e284056907201f9e530ef50c9872c3bf8a090db5
alisyihab <ali.syihab@gmail.com>
2020-04-06 03:45:24 +0700

    perapihan code

--- resources/js/stores/auth.js
+++ resources/js/stores/auth.js
@@ -1,22 +1,17 @@
 import $axios from '../api.js'
+const state = () => ({});
 
-const state = () => ({
-
-});
-
-const mutations = {
-
-};
+const mutations = {};
 
 const actions = {
     submit({ commit }, payload) {
-        localStorage.setItem('token', null)
-        commit('SET_TOKEN', null, { root: true })
+        localStorage.setItem('token', null);
+        commit('SET_TOKEN', null, { root: true });
         return new Promise((resolve, reject) => {
             $axios.post('/login', payload)
             .then((response) => {
-                if (response.data.status == 'success') {
-                    localStorage.setItem('token', response.data.data)
+                if (response.data.status === 'success') {
+                    localStorage.setItem('token', response.data.data);
                     commit('SET_TOKEN', response.data.data, { root: true })
                 } else {
                     commit('SET_ERRORS', { invalid: 'Email/Password Salah' }, { root: true })
@@ -24,7 +19,7 @@ const actions = {
                 resolve(response.data)
             })
             .catch((error) => {
-                if (error.response.status == 422) {
+                if (error.response.status === 422) {
                     commit('SET_ERRORS', error.response.data.errors, { root: true })
                 }
             })

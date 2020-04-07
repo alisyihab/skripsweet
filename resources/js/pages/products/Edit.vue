429640a37e0c439633075145818a18d420a28f88
Ali Syihab <ali.syihab@gmail.com>
2020-04-07 07:30:23 +0700

    page edit laundry produk (ketinggalan hehe)

---
+++ resources/js/pages/products/Edit.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Edit Data Laundry</h4>
+            </div>
+            <div class="card-body">
+                <product-form ref="formProduct"></product-form>
+                <div class="form-group">
+                    <router-link :to="{ name: 'products.data' }" class="btn btn-outline-secondary">
+                        <i class="fas fa-arrow-alt-circle-left"></i>
+                        Kembali
+                    </router-link>
+                    <button class="btn btn-outline-primary" @click.prevent="submit">
+                        <i class="fa fa-save"></i> Update Data
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FormProduct from './Form.vue'
+
+    export default {
+        name: "AddProducts",
+        methods: {
+            submit() {
+                this.$refs.formProduct.submit()
+            }
+        },
+        components: {
+            'product-form': FormProduct
+        }
+    }
+</script>

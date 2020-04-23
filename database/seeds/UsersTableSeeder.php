6eab56749ec3d6ea9cc43c46f5a5bb8172b3efd6
alisyihab <ali.syihab@gmail.com>
2020-04-23 14:55:50 +0700

    fixing seed database saat membuat password

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
             'name' => 'Ali Syihab',
             'email' => 'ali.syihab@gmail.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('secret'),
+            'password' => 'secret',
             'role' => 0
         ]);
     }
